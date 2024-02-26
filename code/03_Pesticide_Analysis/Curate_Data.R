5d3657097ef4a579adad41a29bab972a9d0967be
Spatiotemporal-Exposures-and-Toxicology <messierkp@ns02262008md.ninds.nih.gov.niehs.nih.gov>
2024-02-26 15:44:23 -0500

    addting targets pipeline basics

---
+++ code/03_Pesticide_Analysis/Curate_Data.R
@@ -0,0 +1,7 @@
+## Pesticide Analysis for PrestoGP 
+# We are using the targets package to develop a pipeline for the analysis of pesticide data.
+
+# First, we load the targets package and the tidyverse package.
+library(targets)
+library(tidyverse)
+
